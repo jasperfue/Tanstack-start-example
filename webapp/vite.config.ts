c4b6d4090d9815b9bcf0d007c0a59f86be494bb6
Jasper <jasperfue@live.de>
2025-12-30 19:15:02 +0100

    prettier write

--- webapp/vite.config.ts
+++ webapp/vite.config.ts
@@ -1,10 +1,10 @@
-import { defineConfig } from 'vite'
-import { devtools } from '@tanstack/devtools-vite'
-import { tanstackStart } from '@tanstack/react-start/plugin/vite'
-import viteReact from '@vitejs/plugin-react'
-import viteTsConfigPaths from 'vite-tsconfig-paths'
-import tailwindcss from '@tailwindcss/vite'
-import { nitro } from 'nitro/vite'
+import { defineConfig } from 'vite';
+import { devtools } from '@tanstack/devtools-vite';
+import { tanstackStart } from '@tanstack/react-start/plugin/vite';
+import viteReact from '@vitejs/plugin-react';
+import viteTsConfigPaths from 'vite-tsconfig-paths';
+import tailwindcss from '@tailwindcss/vite';
+import { nitro } from 'nitro/vite';
 
 const config = defineConfig({
   plugins: [
@@ -22,6 +22,6 @@ const config = defineConfig({
       },
     }),
   ],
-})
+});
 
-export default config
+export default config;
